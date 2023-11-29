4ef3391c30e9b3329a95258888819a9e0b025971
Gian-Michele-Cherchi <g.m.cherchi@gmail.com>
2023-11-29 14:44:23 +0100

    debug

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -22,10 +22,12 @@ def subtraj_mode_dataset(
         input_val = df["input_val"]
         target_train = df["target_train"]
         target_val =  df["target_val"]
+        input_test = df["input_test"]
+        target_test = df["target_test"]
     else:   
         data = torch.load(sourcepath, map_location=device)
         data = data.swapaxes(0,1)[:,4000:14000,:,:]
-        norms = [torch.sqrt((data[:,:n_input]**2).mean(dim=(0,1,3))),torch.sqrt((data[:,:n_input].diff(dim=1)**2).mean(dim=(0,1,3)))]
+        norms = [torch.sqrt((data[:,:n_train]**2).mean(dim=(0,1,3))),torch.sqrt((data[:,:n_train].diff(dim=1)**2).mean(dim=(0,1,3)))]
         nsamples = data.size(1)
         n_train = int(split[0]*nsamples)
         n_val = int(split[1]*nsamples)
@@ -53,26 +55,25 @@ def subtraj_mode_dataset(
         input_val = input_val/norms[0][nmode]
         target_train = target_train/norms[1][nmode]
         target_val = target_val/norms[1][nmode]
-        
-    data_train = torch.cat([input_train[:,:n_input], target_train[:,n_input:n_input+1]], dim=1).squeeze(2)
-    data_val = torch.cat([input_val[:,:n_input], target_val[:,n_input:n_input+1]], dim=1).squeeze(2)
+        input_test = input_test/norms[0][nmode]
+        target_test = target_test/norms[1][nmode]
+    
     if test:
         data_test = torch.cat([input_test[:,:n_input], target_test[:,n_input:n_input+1]], dim=1).squeeze(2)
-        
-    for t in range(1,input_train.size(1)-n_input-1):
-        tmp = torch.cat([input_train[:,t:t+n_input], target_train[:,t+n_input:t+n_input+1]], dim=1).squeeze(2)
-        data_train = torch.cat([data_train, tmp], dim=0)
-    for t in range(1,input_val.size(1)-n_input-1):
-        tmp = torch.cat([input_val[:,t:t+n_input], target_val[:,t+n_input:t+n_input+1]], dim=1).squeeze(2)
-        data_val = torch.cat([data_val, tmp], dim=0)
-        
-    if test:
         for t in range(1,input_test.size(1)-n_input-1):
             tmp = torch.cat([input_test[:,t:t+n_input], target_test[:,t+n_input:t+n_input+1]], dim=1).squeeze(2)
             data_test = torch.cat([data_test, tmp], dim=0)
-            return data_train, data_val, data_test, norms
-        
+        return data_test, norms
     else:
+        data_train = torch.cat([input_train[:,:n_input], target_train[:,n_input:n_input+1]], dim=1).squeeze(2)
+        data_val = torch.cat([input_val[:,:n_input], target_val[:,n_input:n_input+1]], dim=1).squeeze(2)
+        for t in range(1,input_train.size(1)-n_input-1):
+            tmp = torch.cat([input_train[:,t:t+n_input], target_train[:,t+n_input:t+n_input+1]], dim=1).squeeze(2)
+            data_train = torch.cat([data_train, tmp], dim=0)
+        for t in range(1,input_val.size(1)-n_input-1):
+            tmp = torch.cat([input_val[:,t:t+n_input], target_val[:,t+n_input:t+n_input+1]], dim=1).squeeze(2)
+            data_val = torch.cat([data_val, tmp], dim=0)
+        
         return data_train, data_val, norms
         
     #data_train = data_train[torch.randperm(data_train.size(0))]
--- src/neural_ar/integrator.py
+++ src/neural_ar/integrator.py
@@ -1,27 +1,32 @@
 import torch 
-from data.utils import *
+from utils import *
 from torch.distributions import MultivariateNormal
-
+import tqdm
 def EulerIntegrator(
                 model, 
                 norms,
                 in_trajectories, 
-                NGENSTEP: int,
+                ngen: int,
                 device: str, 
                 IN_CHANNELS: int=1,
                 **kwargs
                 ):
-    std = kwargs["std"]
+    #std = kwargs["std"]
     #torch.manual_seed(seed)
     INPUT_SEQ = in_trajectories.size(1)
     NPOL = in_trajectories.size(0)
-    gen_data = torch.zeros(NPOL, NGENSTEP+INPUT_SEQ, IN_CHANNELS+1, 3, device=device)
-    mu_gen = torch.ones(NPOL ,NGENSTEP,IN_CHANNELS,3, device=device)
-    sigma_gen = torch.zeros(NPOL, NGENSTEP,IN_CHANNELS,3,3, device=device)
+    gen_data = torch.zeros(NPOL, ngen+INPUT_SEQ, IN_CHANNELS+1, 3, device=device)
+    mu_gen = torch.ones(NPOL ,ngen,IN_CHANNELS,3, device=device)
+    sigma_gen = torch.zeros(NPOL, ngen,IN_CHANNELS,3,3, device=device)
     
     gen_data[:,:INPUT_SEQ,0] = in_trajectories
+    open_log = tqdm.tqdm(total=0, position=0, bar_format='{desc}')
+    open_log.set_description_str(
+        f"Implicit Euler Integration with "+str(ngen)+" steps", 
+        )
+    progress_bar = tqdm.tqdm(total=ngen, desc="Advancement", position=1)
     with torch.no_grad():
-        for t in range(NGENSTEP):
+        for t in range(ngen):
         
             rot_mean_t, _  = model.get_parameters(gen_data[:,t:t+ INPUT_SEQ,0])
            
@@ -35,6 +40,6 @@ def EulerIntegrator(
             trial_sample = torch.bmm(rot_trial_sample[:,None,...], rot_matrix.transpose(1,2))
             gen_data[:,INPUT_SEQ +t,1]  = trial_sample[:,0,:] #sampled velocity
             gen_data[:,INPUT_SEQ +t,0]  = gen_data[:,t+ INPUT_SEQ-1,0]  + norms*trial_sample[:,0,:]  # integrated position
-                
+            progress_bar.update(1)
     return gen_data, mu_gen, sigma_gen
     
