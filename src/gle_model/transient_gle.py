4fd6f39b283e8b3b1444dbf6c3764ef82cf18ff9
Gian-Michele-Cherchi <g.m.cherchi@gmail.com>
2023-11-29 17:23:04 +0100

    Transient GLE done

--- src/gle_model/transient_gle.py
+++ src/gle_model/transient_gle.py
@@ -1,21 +1,18 @@
 
 import torch
 import os
+from dataclasses import dataclass
 from utils import msd_fft
 from sklearn.linear_model import LinearRegression
 
-
+@dataclass
 class TransientGLE():
     """
         Generalized Langevin Equation class modelling transient anomalous diffusion in Polymer Melts 
         with single-polymer center of mass and Normal modes dynamics. 
     """
-     
-    alpha: float
-    beta: float
-    nsamples: int 
-    nframes: int
-    model: object=None
+    
+   
     
     def forward(self, z: torch.Tensor) -> torch.Tensor:
         """
@@ -26,22 +23,23 @@ class TransientGLE():
         OUTPUT 
             - x [batchsize, nframes, 3]: Position Dynamics following GLE fitted solution
         """
-        if self.model is None:
+        if self.reg is None:
             raise ValueError("Paremeters not found. Call fit() first.")
         
         self.nsamples = z.size(0)
         self.nframes = z.size(1)
-        
+        self.alpha = torch.sqrt(self.params)[0][0]
+        self.beta = torch.sqrt(self.params)[0][1]
         bm = torch.randn(self.nsamples, self.nframes, 3).to(z).cumsum(dim=2) # Standard Brownian Motion 
         x = self.alpha*bm + self.beta*z.sum(dim=2)
         return x
     
     def fit(self, 
-            x: torch.Tensor, 
             z:  torch.Tensor, 
-            n_points: int, 
-            t_max: int,
-            t_min: int=None) ->  torch.Tensor:
+            n_max: int,
+            n_min: int=1,
+            n_points: int=3 
+        ) ->  torch.Tensor:
         """
         Fit parameters to modes and center of mass data. 
         
@@ -53,36 +51,35 @@ class TransientGLE():
         OUTPUT:
         """
         
-        if t_min is None:
-            t_min = 1
+
         self.nsamples = z.size(0)
         self.nframes = z.size(1)
-        nmodes = z.size(2)
-        self.params = torch.zeros(nmodes,2)
+        k = z.size(2)
+        assert k>1, ValueError("The number of modes should be at least one.")
+        #self.params = torch.zeros(nmodes,2)
         
-        z = z.swapaxes(2,3).swapaxes(1,2).contiguous().view(self.samples*3,self.nframes,nmodes)
+        modes = z[:,:,1:].swapaxes(2,3).swapaxes(1,2).contiguous().view(self.nsamples*3,self.nframes,k-1)
         
         # linear brownian variance term 
-        t = torch.arange(1,self.nframes).unsqueeze(-1).to(z)
-        f_tau = torch.zeros(self.nframes-t_min, nmodes).to(z)
+        t = torch.arange(n_min,self.nframes).unsqueeze(-1).to(z)
+        f_tau = torch.zeros(self.nframes-n_min, k-1).to(z)
         
         for tau in range(1,self.nframes):
             #f_tau[:,tau-bal_end,:] = (z[:,:NMAX_MODES-(tau)]- z[:,(tau):])[:,0,:]**2
-            f_tau[tau-1,:] = ((z[:,:self.nframes-(tau)]- z[:,(tau):])**2).mean(dim=(0,1))
-        x_msd = msd_fft(x.swapaxes(1,2).contiguous().view(x.size(0)*3,-1)).mean(dim=0)[1:]
+            f_tau[tau-n_min,:] = ((modes[:,:self.nframes-(tau)]- modes[:,(tau):])**2).mean(dim=(0,1))
+        x_msd = msd_fft(z[:,:,0].swapaxes(1,2).contiguous().view(z.size(0)*3,-1)).mean(dim=0)[n_min:]
         
         
-        reg_points = torch.linsapce(t_min, t_max, n_points, dtype=int)
-        self.expl_variance = torch.tensor(nmodes,1)
-        self.model = LinearRegression(positive=True)
-        for mod in range(nmodes):
-            f = torch.cat([t[reg_points], f_tau[reg_points,:mod+1].sum(dim=1, keepdim=True)], dim=1)
-            reg = self.model.fit(f.cpu().numpy(), x_msd[reg_points,None].cpu().numpy())
-            self.params[mod,:] = torch.tensor(reg.coef_)
-            self.expl_variance[mod] = self.params[:,0] + self.params[:,1]*f_tau[reg_points,:mod+1].sum(dim=1, keepdim=True)
+        reg_point_index = torch.linspace(0,n_max, n_points, dtype=int)
+        self.reg = LinearRegression(positive=True)
+        
+       
+        f = torch.cat([t[reg_point_index], f_tau[reg_point_index].sum(dim=1, keepdim=True)], dim=1)
+        reg = self.reg.fit(f.cpu().numpy(), x_msd[reg_point_index,None].cpu().numpy())
+        self.params = torch.tensor(reg.coef_)
     
     def get_params(self) -> torch.Tensor:
-        if self.model is None:
+        if self.reg is None:
             raise ValueError("Model not fitted yet. Call fit() first.")
         return self.params
     
