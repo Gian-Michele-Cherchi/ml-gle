9b2144edd29c14d8f479ee6eae4d2242725a2903
Gian-Michele-Cherchi <g.m.cherchi@gmail.com>
2023-11-29 14:44:06 +0100

    data loading modifications

--- src/run_gen.py
+++ src/run_gen.py
@@ -62,7 +62,7 @@ def eval_app(cfg: DictConfig) -> None:
     k = cfg.eval["k"]
     flag_acf = cfg.eval["acf"]
     flag_gle = cfg.eval["gle"]
-    
+    n_train = int(per_train*10000)
     # NAR model 
     model = dynamical_model(
                         input_length=n_input, 
@@ -71,8 +71,11 @@ def eval_app(cfg: DictConfig) -> None:
                         pred_var_dims=3,
                         ).to(DEVICE)
 
-    modes_traj = torch.Tensor(nsamples, ngen, k, 3).to(DEVICE)
-    genmode_corr = []
+    modes_traj = torch.Tensor(nsamples, ngen+n_input, k, 3).to(DEVICE)
+    gen_nacfs = {}
+    sourcepath = os.path.join(DATAPATH, "ready/modesdata_T"+str(temp)+"_.pt")
+    test_data = torch.load(sourcepath, map_location=DEVICE).swapaxes(0,1)
+    norms = [torch.sqrt((test_data[:,:n_train]**2).mean(dim=(0,1,3))),torch.sqrt((test_data[:,:n_train].diff(dim=1)**2).mean(dim=(0,1,3)))]
     
     for nmode in range(1,k):
         
@@ -82,67 +85,69 @@ def eval_app(cfg: DictConfig) -> None:
         
         try:
             losspath = os.path.join(savepath, "metrics.csv")
+            train_metrics = pd.read_csv(losspath)
         except OSError as e:
             print(e.errno)
-        train_metrics = pd.read_csv(losspath)
+        
         best_epoch = np.argmin(train_metrics["Val"].values)+1
         #if nmode == 1:
         #    best_epoch = 24
         #if nmode == 2:
         #    best_epoch = 21
             #print(epoch)
-        mode_filepath = os.path.join(gen_path, "gen_data_mode"+str(nmode)+"_epoch"+str(best_epoch)+"_.pt")
+        mode_filepath = os.path.join(gen_path, "gen_mode_epoch"+str(best_epoch)+"_.pt")
         try: 
             # If file exists, load in memory instead of generating 
             modes_traj[:, :, nmode] = torch.load(mode_filepath, map_location=DEVICE)[:,:ngen,0]
         # Generate mode dynamics 
         except OSError as e:
             print("[File not Found. Starting Mode "+str(nmode)+" Autoregressive Generation with model at epoch "+str(best_epoch)+"]\n")
-            sourcepath = os.path.join(DATAPATH, "ready/modesdata_T"+str(temp)+"_.pt")
-            filepath = os.path.join(DATAPATH, "subtraj/subtraj_ds_T"+str(temp)+"_"+str(n_input)+"steps_mode"+str(nmode)+"_.pt")
-            _, _, test_data, norms = subtraj_mode_dataset(
-                                                    sourcepath=sourcepath,
-                                                    filepath=filepath,
-                                                    n_input=n_input,
-                                                    split=[per_train, per_train + per_val*per_train], 
-                                                    nmode=nmode, 
-                                                    norm=True,
-                                                    device=DEVICE,
-                                                    test=True
-                                                    )
+            #test_data, norms = subtraj_mode_dataset(
+            #                                    sourcepath=sourcepath,
+            #                                    filepath=filepath,
+            #                                    n_input=n_input,
+            #                                    split=[per_train, per_train + per_val*per_train], 
+            #                                    nmode=nmode, 
+            #                                    norm=True,
+            #                                    device=DEVICE,
+            #                                    test=True
+            #                                    )
             
-            CHECKPTFILE = "checkpt_epoch_"+str(best_epoch)+"_.pth"
+            # Load model weights 
+            checkpt_file = "checkpt_epoch_"+str(best_epoch)+".pth"
             #BESTCKPTFILE = "checkpt_best_.pth"
-            CHECKPTPATH = os.path.join(SAVEPATH,CHECKPTFILE)
-            checkpoint = torch.load(CHECKPTPATH)
+            checkpt_path = os.path.join(savepath,checkpt_file)
+            checkpoint = torch.load(checkpt_path)
             model.load_state_dict(checkpoint)
             model.eval()
             
             # Trained Autoregressive model embedded in an Implicit Euler Integrative Scheme
             mode_traj, mu_traj, sigma_traj  = EulerIntegrator( 
-                model, 
-                norms=norms,
-                in_trajectories=test_data[:nsamples,:n_input],
-                ngen=ngen,
-                device=DEVICE,
-                IN_CHANNELS=1,
-                std=norms[0][nmode]
-                )
-            modes_traj[:,:,nmode] = mode_traj[0]
+                                                        model, 
+                                                        norms=norms[1][nmode]/norms[0][nmode],
+                                                        in_trajectories=test_data[:,n_train: n_train + n_input,nmode],
+                                                        ngen=ngen,
+                                                        device=DEVICE,
+                                                        IN_CHANNELS=1,
+                                                        )
+            modes_traj[:,:,nmode] = mode_traj[:,:,0]
             if flag_acf:
-               #genmode_corr.append(autocorrFFT(gen_data[:,:,0].swapaxes(1,2).contiguous().view(3*gen_data.size(0),-1)).mean(dim=0)/gen_data[:,:,0].var())
-               pass
-               
+                tmp = {}
+                acf = autocorrFFT(mode_traj[:,:,0].swapaxes(1,2).contiguous().view(mode_traj[:,:,0].size(0)*3,-1))/mode_traj[:,:,0].var()
+                tmp["nacfs"] = acf.mean(dim=0)
+                tmp["SE"] = torch.tensor(list(map(lambda i: acf_se(acf[:,:i],acf.size(1)), range(1,acf.size(1)))))
+                gen_nacfs["mode"+str(nmode)] = tmp   
+                torch.save(os.path.join(gen_path, "gen_mode_nacfs_T"+str(temp)+"_ngen"+str(ngen)+"_.pt"))
+                
             torch.save(mode_traj,mode_filepath)
-            torch.save(mu_traj, os.path.join(gen_path, "gen/gen_mu_mode"+str(nmode)+"_epoch"+str(best_epoch)+"_.pt"))
-            torch.save({"mu": mu_traj, "sigma": sigma_traj}, os.path.join(gen_path, "gen/gen_params_mode"+str(nmode)+"_epoch"+str(best_epoch)+"_.pt"))
+            torch.save({"mu": mu_traj, "sigma": sigma_traj}, os.path.join(gen_path, "gen_params_mode"+str(nmode)+"_epoch"+str(best_epoch)+"_.pt"))
             
             eval_metrics = {"mode": nmode,
                             "Best Epoch": best_epoch,
                             "params": params,
                             }
             wandb.log(eval_metrics)
-        
+            pd.save_csv(os.path.join(gen_path,"eval_metrics.csv"))
     if flag_gle: 
         # Fit GLE model on short trajectories 
         gle_model = TransientGLE()
@@ -152,6 +157,7 @@ def eval_app(cfg: DictConfig) -> None:
         # Center of Mass dyamics with Transient GLE solution and generated modes 
         modes_traj[:,:,0] = gle_model(modes_traj)
     
+        
        
 if __name__ == "__main__":
     eval_app()
